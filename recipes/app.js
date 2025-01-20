6436910785d2f68ec76f025ce85be5dcb239791c
shajahan1299 <shajahancse1299@gmail.com>
2025-01-20 17:46:59 +0530

    recipe

--- recipes/app.js
+++ recipes/app.js
@@ -15,7 +15,7 @@ app.use("/api/recipes", recipeRoutes);
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(process.env.PORT, () => {
